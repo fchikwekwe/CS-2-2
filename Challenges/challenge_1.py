db533786a01fc2c1cc0555b44999b7782ac011de
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-07-15 16:55:17 -0700

    🌟 refactored code for proper output

--- Challenges/challenge_1.py
+++ Challenges/challenge_1.py
@@ -3,19 +3,34 @@
 #         self.name = name
 #         self.neighbors = []
 
+
 class Graph():
+    """
+    
+    """
     def __init__(self, graph_file):
+        """
+        Initializes graph and loads file to read from. 
+        """
         self.file = graph_file
 
     def vertex_count(self):
+        """
+        Input: None; reads from self.file
+        Output: Number of vertices present in graph
+        """
         vertices = 0
         with open(self.file, 'r') as file:
             for line in file.readlines():
-                if line[0] == '(' and len(line) >= 5:
-                    vertices += 1
+                if line[0].isalnum() and line[0] != 'G' and line != 'D':
+                    return len(line.split(','))
             return vertices
 
     def edge_count(self):
+        """
+        Input: None; reads from self.file 
+        Ouput: Number of edges present in graph
+        """
         edges = 0
         with open(self.file, 'r') as file:
             for line in file.readlines():
@@ -24,32 +39,28 @@ class Graph():
             return edges
 
     def edge_list(self):
+        """
+        Input: None; reads from self.file
+        Output: List of tuples with edges and (if necessary) weights
+        """
         edges = []
         with open(self.file, 'r') as file:
             for line in file.readlines():
                 if line[0] == '(' and len(line) >= 5:
                     edges.append((line.strip('\n')))
-        return edges
-        # return self.clean_edges(edges)
+        return self.clean_edges(edges)
 
     def clean_edges(self, edges):
-        for edge in edges:
-            new_edge = []
-            num = ''
-            index = 0
+        """
+        Input: string representation of list of edges
+        Output: Actual list representation of list of edges
+        """
+        import ast
 
-            for char in edge:
-                if char.isdigit():
-                    num += char
-                    index += 1
-                else: 
-                    if num: 
-                        print()
-                        new_edge.append(int(num))
-                        num = ''
-                        edge = new_edge
-                    index += 1
-        return edges
+        clean_edges = []
+        for edge in edges: 
+            clean_edges.append(ast.literal_eval(edge))
+        return clean_edges
 
 def main():
     import sys 
